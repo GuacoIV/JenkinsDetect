ca6ddc3123833a8cb40d4bca71612c976a27fa1f
GuacoIV <edward.lynch@att.net>
2014-04-06 21:29:44 -0500

    Live graphing every 512 samples

--- JenkinsDetect.m
+++ JenkinsDetect.m
@@ -2,10 +2,13 @@
 [y, Fs] = audioread('Jenkins (1).wav');
 % Fs = sample rate
 L = length(y);
-%for i = 1:L/(Fs*.5)
+%for i = 1:L/512
+i=30;
 samples = [1,Fs*.5];
-[y, Fs] = audioread('Jenkins (1).wav', samples);
-sound(y, Fs);
+z = y(1+(512*i):512*i);
+%[y, Fs] = audioread('Jenkins (1).wav', samples);
+sound(z, Fs);
 plot(abs(fft(y)));
-clear y
+pause(0.1);
+%clear y
 %end
