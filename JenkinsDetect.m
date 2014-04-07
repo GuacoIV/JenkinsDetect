4db3db40ea9cedb96310a8e8e014c288137d78e4
GuacoIV <edward.lynch@att.net>
2014-04-06 22:58:27 -0500

    Detects E!

--- JenkinsDetect.m
+++ JenkinsDetect.m
@@ -1,12 +1,16 @@
 %Profile a J
 [y, Fs] = audioread('J1.wav');
-L = length(y);
 NFFT = 2^nextpow2(512);
-f = Fs/2*linspace(0,1,NFFT/2+1);
 resultJ = 2*abs(y(1:NFFT/2+1));
 foundJ = 0;
 
-[y, Fs] = audioread('Jenkins (7).wav');
+%Profile an e
+[y, Fs] = audioread('E1.wav');
+NFFT = 2^nextpow2(512);
+resultE = 2*abs(y(1:NFFT/2+1));
+foundE = 0;
+
+[y, Fs] = audioread('Jenkins (1).wav');
 % Fs = sample rate
 L = length(y);
 count = 0;
@@ -24,6 +28,13 @@ if (2*abs(z(1:NFFT/2+1)) - resultJ < .0007)
     display(count);
     display(answer);
     foundJ = 1;
+end
+if (foundJ == 1 & abs(2*abs(z(1:NFFT/2+1)) - resultE) < .2)
+    answer = 'Found an E!';
+    count=count+1;
+    display(count);
+    display(answer);
+    foundE = 1;
 end
 pause(0.05);
 end
