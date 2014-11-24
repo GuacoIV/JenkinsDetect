15a3abe3aebcaec0c6b7817c6408ac981c382f96
GuacoIV <edward.lynch@att.net>
2014-11-24 00:40:21 -0600

    Accurate!  So freaking accurate!

--- Jenkinsmfcc.m
+++ Jenkinsmfcc.m
@@ -1,26 +1,62 @@
-accuracy = 14;
+Profiler;
+accuracy = 5;
 [y, Fs] = audioread('Jenkins (1).wav');
 one = melcepst(y, 44100, 'M', 12, floor(3*log(44100)), 512, 512/2, 0, 0.5);
 
  for i = 1:11
-     whichOne = i;
+    whichOne = i;
     strwhichOne = strcat('Jenkins (', num2str(whichOne), ').wav');
     [y, Fs] = audioread(strwhichOne);
     sound(y, 44100);
-    two = melcepst(y, 44100, 'M', 12, floor(3*log(44100)), 512, 512/2, 0, 0.5);
-    sizeOne = size(one);
-    sizeTwo = size(two);
-    if (sizeOne > sizeTwo)
-        smalestSize = sizeTwo;
-    else
-        smallestSize = sizeOne;
-    end
-    result = one(1:smallestSize) - two (1:smallestSize);
-    max(result)
-    min(result)
-    if (abs(result) < accuracy)
-        answer = 'Yes!'
-    else answer = 'No!'
+    foundJ = 0;
+    foundE = 0;
+    foundN = 0;
+    foundK = 0;
+    foundI = 0;
+    foundS = 0;
+    for j = 1:(size(y, 1)/512)
+        resultNow = melcepst(y((1+((j-1)*512)):(j*512), :), 44100, 'M', 12, floor(3*log(44100)), 512, 512/2, 0, 0.5);
+        if (foundJ == 0)
+            result = resultJ - resultNow;
+            if (abs(result) < accuracy)
+                foundJ = 1;
+            end
+        elseif (foundE == 0)
+            result = resultE - resultNow;
+             if (abs(result) < accuracy)
+                foundE = 1;
+            end
+        elseif (foundN == 0)
+            result = resultN - resultNow;
+             if (abs(result) < accuracy)
+                foundN = 1;
+            end
+        elseif (foundK == 0)
+            result = resultK - resultNow;
+             if (abs(result) < accuracy)
+                foundK = 1;
+            end
+        elseif (foundI == 0)
+            result = resultI - resultNow;
+             if (abs(result) < accuracy)
+                foundI = 1;
+            end
+        elseif (foundS == 0)
+            result = resultS - resultNow;
+             if (abs(result) < accuracy)
+                foundS = 1;
+            end
+        end
+        if (j == round((size(y, 1)/512)) - 1)
+            if (foundJ == 1 && foundE == 1 && foundN == 1 && foundK == 1 && foundI == 1 && foundS == 1)
+                answer = 'Yes'
+            else
+                answer = 'No'
+            end
+        end
+        max(result);
+        min(result);
+
     end
     pause(1.5);
  end
@@ -28,22 +64,57 @@ one = melcepst(y, 44100, 'M', 12, floor(3*log(44100)), 512, 512/2, 0, 0.5);
  filesToTest = {'EdJenkins1.wav', 'EdJenkinsFast.wav', 'EdJenkinsFast.wav', 'EdJenkinsLoud.wav', 'Jennifer.wav', 'Jenny.wav', 'Jerry.wav', 'Jerry2.wav', 'JimBob.wav', 'Gingerbread.wav', 'Turn.wav', 'Store.wav', 'Cupcake.wav', 'Alarm.wav'};
 
 for i = 1 : (size(filesToTest, 2))
-    [y, Fs] = audioread(char(filesToTest(i)));
+   [y, Fs] = audioread(char(filesToTest(i)));
     sound(y, 44100);
-    two = melcepst(y, 44100, 'M', 12, floor(3*log(44100)), 512, 512/2, 0, 0.5);
-    sizeOne = size(one);
-    sizeTwo = size(two);
-    if (sizeOne > sizeTwo)
-        smalestSize = sizeTwo;
-    else
-        smallestSize = sizeOne;
-    end
-    result = one(1:smallestSize) - two (1:smallestSize);
-    max(result)
-    min(result)
-    if (abs(result) < accuracy)
-        answer = 'Yes!'
-    else answer = 'No!'
+    foundJ = 0;
+    foundE = 0;
+    foundN = 0;
+    foundK = 0;
+    foundI = 0;
+    foundS = 0;
+    for j = 1:(size(y, 1)/512)
+        resultNow = melcepst(y((1+((j-1)*512)):(j*512), :), 44100, 'M', 12, floor(3*log(44100)), 512, 512/2, 0, 0.5);
+        if (foundJ == 0)
+            result = resultJ - resultNow;
+            if (abs(result) < accuracy)
+                foundJ = 1;
+            end
+        elseif (foundE == 0)
+            result = resultE - resultNow;
+             if (abs(result) < accuracy)
+                foundE = 1;
+            end
+        elseif (foundN == 0)
+            result = resultN - resultNow;
+             if (abs(result) < accuracy)
+                foundN = 1;
+            end
+        elseif (foundK == 0)
+            result = resultK - resultNow;
+             if (abs(result) < accuracy)
+                foundK = 1;
+            end
+        elseif (foundI == 0)
+            result = resultI - resultNow;
+             if (abs(result) < accuracy)
+                foundI = 1;
+            end
+        elseif (foundS == 0)
+            result = resultS - resultNow;
+             if (abs(result) < accuracy)
+                foundS = 1;
+            end
+        end
+        if (j == round((size(y, 1)/512)) - 1)
+            if (foundJ == 1 && foundE == 1 && foundN == 1 && foundK == 1 && foundI == 1 && foundS == 1)
+                answer = 'Yes'
+            else
+                answer = 'No'
+            end
+        end
+        max(result);
+        min(result);
+
     end
     pause(1.5);
 end
